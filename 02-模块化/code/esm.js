e6386e0f9aaedbb68b25547f7eee64340f4f8025
Tony <1821955020@qq.com>
2024-11-18 23:31:08 +0800

    Delete 02-模块化 directory

--- 02-模块化/code/esm.js
+++
@@ -1,7 +0,0 @@
-// import * as all from './test.js'
-// console.log(all);
-
-// esm不支持引入json文件
-// nodejs18 以上的高版本可以使用asser断言指定引入文件的类型
-import json from './data.json' assert {type: "json"}
-console.log(json);
