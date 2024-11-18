c71398f1edcd92cb902598d0c6748835476eeefb
Tony <1821955020@qq.com>
2024-11-18 23:32:43 +0800

    Delete 12-pngquant directory

--- 12-pngquant/code/index.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('node:child_process');
-
-// exec('pngquant test.png --output test-out.png')
-
-// --quality 取值0-100，数值越大，图片质量越高，体积大  数值越小，图片质量越低，体积小
-// exec('pngquant test.png --quality=80 --output test-out-quality.png')
-
-// --speed 取值1-10，数值越大，压缩速度越快，质量低  数值越小，压缩速度越慢，质量高
-exec('pngquant test.png --speed=1 --quality=80 --output test-out-speed.png')
