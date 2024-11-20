24bb0c9439f6c2293db1478c56d6904a093dd9d8
cheeseburgertony <1821955020@qq.com>
2024-11-20 20:29:39 +0800

    feat: Buffer的其他创建方式

---
+++ 03-Node中的类-Buffer/02-Buffer的其他创建方式.js
@@ -0,0 +1,19 @@
+// 1.创建一个Buffer对象
+// 8个字节的buffer内存空间
+const buf = Buffer.alloc(8);
+console.log(buf);
+
+// 2.手动对每个字节进行访问
+console.log(buf[0]);
+console.log(buf[1]);
+
+// 3.手动对每个字节进行操作
+buf[0] = 100;
+buf[1] = 0x66;
+console.log(buf);
+console.log(buf.toString());
+
+buf[2] = 'm'.charCodeAt()
+console.log(buf);
+
+
